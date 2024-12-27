c25f414907a5ae98c9649d07ef2903297b6f8762
Swain, Susrut (Cognizant) <Susrut.Swain@otiselevator.net>
2024-12-27 17:58:58 +0530

    Initial commit

---
+++ MAUICalculator/obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("MAUICalculator.MainApplication, MAUICalculator, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6499d73a09ad0fea2e.MainApplication.class, crc6499d73a09ad0fea2e.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
